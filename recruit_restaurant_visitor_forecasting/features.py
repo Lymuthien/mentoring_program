763dda3a00b28ede919d401dc318f99831a490e6
lymuthien <tanya120606@mail.ru>
2025-11-27 13:25:51 +0300

    Removed CLI

--- recruit_restaurant_visitor_forecasting/features.py
+++ recruit_restaurant_visitor_forecasting/features.py
@@ -295,23 +295,3 @@ def add_total_neigh_reserves(df_visit: pd.DataFrame, hpg_res: pd.DataFrame, regi
     df.drop([RESERVE_HPG_NEIGHBORS, RESERVE_AIR_NEIGHBORS], axis=1, inplace=True)
 
     return df
-
-
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Features generation complete.")
-    # -----------------------------------------
-
-
-if __name__ == "__main__":
-    app()
--- recruit_restaurant_visitor_forecasting/plots.py
+++ recruit_restaurant_visitor_forecasting/plots.py
@@ -252,23 +252,3 @@ def plot_acf(acf_df: pd.DataFrame, col: str, df_str: str) -> None:
     plt.xlabel("Lag")
     plt.ylabel("ACF")
     plt.title(f"{df_str} - {col} - ACF")
-
-
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = FIGURES_DIR / "plot.png",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating plot from data...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Plot generation complete.")
-    # -----------------------------------------
-
-
-if __name__ == "__main__":
-    app()
