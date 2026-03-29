3d1bcc27fdd7e76e0a7ed61d994d775af0aa3939
lymuthien <tanya120606@mail.ru>
2026-03-29 20:48:27 +0300

    Shown feature correlation difference before and after clusterization.

--- recruit_restaurant_visitor_forecasting/plots.py
+++ recruit_restaurant_visitor_forecasting/plots.py
@@ -524,15 +524,15 @@ def plot_errors_boxplot(
     plt.tight_layout()
 
 
-def plot_corr_matrix(corr_df: pd.DataFrame):
+def plot_corr_matrix(corr_df: pd.DataFrame, max_abs: float = 1):
     fig = px.imshow(
         corr_df,
         labels=dict(x="feature", y="feature", color="corr"),
         x=corr_df.columns,
         y=corr_df.columns,
         color_continuous_scale="RdBu",
-        zmin=-1,
-        zmax=1,
+        zmin=-max_abs,
+        zmax=max_abs,
     )
     fig.update_layout(width=900, height=800)
     fig.show()
